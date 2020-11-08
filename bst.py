d3effc936bc69ad4568889f80141628252496cbf
Nick <morrinic@oregonstate.edu>
2020-11-07 20:41:32 -0800

    Test instantiation works

--- bst.py
+++ bst.py
@@ -3,7 +3,9 @@
 # If you can't figure it out recursively, use a loop. (But then refactor
 # your implementation into a recursive one!)
 # Your implementation should pass the tests in test_bst.py.
-# YOUR NAME
+# Nick Morris
 
 class BinarySearchTree:
-   pass
+   
+   def __init__(self):
+      pass
